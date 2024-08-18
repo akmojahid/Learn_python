66c746b26ed0410e2d8db2a3e5c65538ca7c5107
akmojahid <texthouse7@gmail.com>
2024-08-18 18:58:13 +0600

    some files aded

--- python/code.py
+++ python/code.py
@@ -1,1 +1,5 @@
-print("Hello, MAmun! ")
+import utils
+
+utils.my_name("Mujahid")
+
+add(5, 2)
---
+++ python/utils.py
@@ -0,0 +1,6 @@
+def my_name(name):
+  print("My name is ", name)
+  
+def add(a, b):
+  sum = a + b
+  print(sum)
